07eba369623d08beee8ccb2037731a67f99157a1
Jed Peek <jedpeek@yahoo.com>
2018-04-17 15:08:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string){
   if(string === string.toLowerCase()){
     return "I can't hear you!"
   }
-  if(string === string.toUpperCase()){
+  elsif(string === string.toUpperCase()){
     return "YES INDEED!"
   }
   else(string === "I love you, Grandma."){
